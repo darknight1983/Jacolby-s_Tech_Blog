eafa020d37dd82c5bb68ca5aaa2b17d19d9ea516
Jacolby Green <darknightnov@gmail.com>
2021-09-17 15:13:17 -0500

    adding pages in gatsby

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+
+const AboutMePage = () => {
+    return (
+        <main>
+            <title>About Me</title>
+            <h1>This will be the About Me page</h1>
+            <p>Im building this by following the tutorial on Gatsby's website</p>
+        </main>
+    )
+}
+
+
+export default AboutMePage;
